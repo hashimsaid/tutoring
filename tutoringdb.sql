443d8f62f360a8d2a00bd585d4832f8612e7eed2
bilal-tamer <bilal.ghareeb@gmail.com>
2022-01-15 02:56:14 +0200

    Modified DB

--- tutoringdb.sql
+++ tutoringdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2022 at 12:56 AM
+-- Generation Time: Jan 15, 2022 at 01:55 AM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 7.3.31
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -133,6 +133,67 @@ CREATE TABLE `materials` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(100) NOT NULL,
+  `sent_by` int(11) NOT NULL,
+  `received_by` int(11) NOT NULL,
+  `message` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `createdAt` varchar(255) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `sent_by`, `received_by`, `message`, `createdAt`) VALUES
+(77, 13, 14, 'aaaa', '2022-01-08 09:07:58pm'),
+(78, 13, 14, 'hi', '2022-01-08 09:09:40pm'),
+(79, 13, 14, 'hi', '2022-01-08 09:09:46pm'),
+(80, 13, 14, 'a7a', '2022-01-08 09:11:13pm'),
+(81, 13, 14, 'a7a', '2022-01-08 09:11:18pm'),
+(82, 13, 11, 'hi', '2022-01-08 11:14:28pm'),
+(83, 13, 11, 'sad', '2022-01-08 11:14:38pm'),
+(84, 13, 14, 'sad', '2022-01-08 11:23:30pm'),
+(85, 13, 14, 'alo', '2022-01-08 11:23:40pm'),
+(86, 13, 14, 'hla', '2022-01-08 11:23:55pm'),
+(87, 13, 14, 'alo', '2022-01-08 11:24:02pm'),
+(88, 13, 14, 'aaaa', '2022-01-08 11:24:12pm'),
+(89, 13, 14, 'sad', '2022-01-08 11:24:14pm'),
+(90, 13, 14, 'hi', '2022-01-08 11:24:17pm'),
+(91, 14, 13, 'fine', '2022-01-08 11:25:12pm'),
+(92, 13, 14, 'zby', '2022-01-08 11:34:04pm'),
+(93, 14, 13, 'kosomk', '2022-01-08 11:34:10pm'),
+(94, 13, 11, 'hi', '2022-01-15 12:36:55am'),
+(95, 13, 11, 'alo', '2022-01-15 12:36:57am'),
+(96, 13, 11, 'hla', '2022-01-15 12:42:53am'),
+(97, 11, 13, '????', '2022-01-15 12:43:00am'),
+(98, 11, 13, 'hello world', '2022-01-15 12:43:20am'),
+(99, 13, 11, 'hi', '2022-01-15 12:43:30am'),
+(100, 13, 11, 'hi', '2022-01-15 12:46:27am'),
+(101, 11, 13, 'hello world', '2022-01-15 12:46:28am'),
+(102, 11, 13, 'hello world', '2022-01-15 12:46:45am'),
+(103, 13, 11, 'aaaa', '2022-01-15 12:47:06am'),
+(104, 11, 13, 'fine', '2022-01-15 12:47:12am'),
+(105, 13, 11, 'aaaa', '2022-01-15 12:47:22am'),
+(106, 13, 11, 'hla', '2022-01-15 12:47:28am'),
+(107, 11, 13, '????', '2022-01-15 12:47:33am'),
+(108, 11, 13, '????', '2022-01-15 12:47:54am'),
+(109, 13, 11, 'hla', '2022-01-15 12:47:55am'),
+(110, 13, 11, 'sad', '2022-01-15 12:48:04am'),
+(111, 13, 11, 'aaaa', '2022-01-15 12:48:12am'),
+(112, 13, 11, 'a7a', '2022-01-15 12:48:16am'),
+(113, 13, 11, 'hi', '2022-01-15 12:48:20am'),
+(114, 11, 13, 'hello world', '2022-01-15 12:48:24am'),
+(115, 11, 13, 'hello world', '2022-01-15 12:48:28am'),
+(116, 1, 2, 'hi', '2022-01-15 01:27:09am'),
+(117, 2, 1, 'fine', '2022-01-15 01:27:51am');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `orders`
 --
 
@@ -153,7 +214,15 @@ INSERT INTO `orders` (`orderID`, `courseID`, `learnerID`) VALUES
 (937556936, 1, 2),
 (937556936, 0, 2),
 (456430094, 0, 2),
-(206761981, 0, 1);
+(206761981, 0, 1),
+(1662757592, 0, 1),
+(1662757592, 1, 1),
+(959431989, 0, 1),
+(959431989, 1, 1),
+(1194711523, 0, 1),
+(1194711523, 1, 1),
+(2068024582, 0, 1),
+(2068024582, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -174,7 +243,8 @@ CREATE TABLE `selectedcourses` (
 
 INSERT INTO `selectedcourses` (`courseID`, `learnerID`, `rating`, `review`) VALUES
 (0, 1, 1, 'bad'),
-(0, 2, 5, 'test');
+(0, 2, 5, 'test'),
+(1, 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -235,6 +305,14 @@ ALTER TABLE `learners`
   ADD PRIMARY KEY (`learnerID`);
 
 --
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sent_by` (`sent_by`,`received_by`),
+  ADD KEY `received_by` (`received_by`);
+
+--
 -- Indexes for table `selectedcourses`
 --
 ALTER TABLE `selectedcourses`
@@ -269,6 +347,12 @@ ALTER TABLE `auditors`
 --
 ALTER TABLE `learners`
   MODIFY `learnerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 
 --
 -- AUTO_INCREMENT for table `tutors`
