00416724ce1dcec8da8b5999546e8c22ac24db11
Hashim <96691482+hashimsaid@users.noreply.github.com>
2022-01-15 18:07:02 +0200

    Add files via upload

--- addcourses.php
+++ addcourses.php
@@ -1,15 +1,25 @@
 <html>
-    <h2> Add courses </h2>
-
+    <h2> Request submitted </h2>
+    <br> 
+    <h4> course has been sent to admin<h4>
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "tutoringdb";
 $conn = new mysqli($servername, $username, $password, $dbname);
-$query = INSERT INTO `courses`(`courseID`,`courseName`,`averageRating`,`price`,`description`,`picture`,`approved`) VALUES ('','','','','','','');;
+
+//variables gotten
+
+$cName=$_POST['courseName'];
+$price=$_POST['price'];
+$desc=$_POST['desc'];
+$picture=$_POST['picture'];
+$notapproved = 0;
+$query = "INSERT INTO courses (courseName,price,description,picture,approved) 
+VALUES ('$cName','$price','$desc','$picture','$notapproved')";
 $result = mysqli_query($conn ,$query);
+
 ?>
 
 </html>
---
+++ getmaterials.php
@@ -0,0 +1,10 @@
+<html>
+    <h3> Add Materials </h3>
+<form method='post' action='addmaterials.php' > 
+CourseID : <input type = 'text'  name= "courseID" >
+<br>
+File : <input type = 'file' name="path">
+submit : <input type = 'submit' >
+</form>
+
+</html>
