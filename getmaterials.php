9327b4810f2be77af3351330c50fd4902fc00df7
Hashim <96691482+hashimsaid@users.noreply.github.com>
2022-01-16 21:08:50 +0200

    Update getmaterials.php

--- getmaterials.php
+++ getmaterials.php
@@ -1,12 +1,20 @@
+
+
 <html>
-    <h3> Add Materials </h3>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>   
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+<h3 class="bg-success p-2 text-dark bg-opacity-10"> Add Materials </h3>
 
   <form action="" method="post" name="edit" enctype="multipart/form-data">
-CourseID : <input type = 'text'  name= "courseID" >
-<br>
-File : <input type = 'file' name='path'>
+CourseID : <input type = 'text'  name= "courseID"  >
 
-submit : <input type = 'submit' name="upload">
+<br><br>
+ <input type = 'file' name='path' id="formFile" class="form-control" >
+<br>
+ <input type = 'submit' class="btn btn-primary" name="upload">
 
 </form>
 
@@ -32,9 +40,7 @@ if ($_FILES['path']['size'] == 0 && $_FILES['path']['error'] == 0) {
 
 }
 else{
-    echo $target_file ;
-    echo $name;
-    echo $cID;
+    
         $sql = "INSERT INTO materials (courseID,materialPath) VALUES ('$cID','$target_file')";
        // $sql="INSERT INTO materials (file_name, type, size) VALUES('$final_file', '$file_type','$new_size')";
            mysqli_query($conn,$sql);
@@ -45,5 +51,23 @@ else{
     move_uploaded_file($tmp_name, "$target_dir/$name");
  
 }
+
 ?>
+
+<!-- <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form> -->
 </html>
