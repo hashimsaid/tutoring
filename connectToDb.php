cc888c389725d1d08af6b0c1b1df4ca43f3de4bc
Hashim <96691482+hashimsaid@users.noreply.github.com>
2022-01-21 18:21:49 +0200

    error handling

--- connectToDb.php
+++ connectToDb.php
@@ -1,9 +1,13 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "tutoringdb";
 
+try{
 $conn = new mysqli($servername, $username, $password, $dbname);
+}
 
+catch(PDOException $e){die($e->getMessage ()); }
 ?>
