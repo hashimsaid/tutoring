fe7d2fe6a160919d48f2f94c85f1a69881cdb4d5
bilal-tamer <bilal.ghareeb@gmail.com>
2022-01-22 03:48:34 +0200

    new DB

--- tutoringdb.sql
+++ tutoringdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2022 at 10:05 PM
+-- Generation Time: Jan 22, 2022 at 02:47 AM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 7.3.31
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,7 +41,6 @@ CREATE TABLE `adminstrators` (
 --
 
 INSERT INTO `adminstrators` (`adminID`, `Fname`, `Lname`, `Email`, `Password`, `Type`) VALUES
-('61eaba784da34', 'shihab', 'Admin', 'shihab.admin@gmail.com', '$2y$10$EkM/Bpx6Jn1509Eju5j.P.0U7Hec5w0QCZWbiiGatdNCS0MLAM94.', 'adminstrator'),
 ('61eaba8c4aac3', 'bilal', 'Admin', 'Bilal.admin@gmail.com', '$2y$10$uUYd0kub2./A0rLDOID5KeQqazP71DLB6hV4mb.7lnC0pZW20IcPO', 'adminstrator');
 
 -- --------------------------------------------------------
@@ -99,8 +98,9 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`courseID`, `courseName`, `averageRating`, `price`, `description`, `picture`, `approved`, `tutorID`) VALUES
-(3, 'Data Structures', '0', 5000, 'ay haga', 'assassins_creed_iv_black_flag_edward_kenway_weapons_crows_battle_97582_1920x1080.jpg', 1, 2),
-(4, 'Data Structures 2', '0', 100000, 'ay haga 2', 'azwraith_phantom_lancer_dota_2_art_94278_1920x1080.jpg', 1, 2);
+(3, 'Data Structures', '4', 5000, 'ay haga', 'assassins_creed_iv_black_flag_edward_kenway_weapons_crows_battle_97582_1920x1080.jpg', 1, 2),
+(4, 'Data Structures 2', '0', 100000, 'ay haga 2', 'azwraith_phantom_lancer_dota_2_art_94278_1920x1080.jpg', 1, 2),
+(5, 'React JS', '0', 7000, 'react js course for noobs', 'React.jfif', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -125,7 +125,8 @@ CREATE TABLE `learners` (
 INSERT INTO `learners` (`learnerID`, `Fname`, `Lname`, `Email`, `Password`, `profilePicture`, `Type`) VALUES
 ('61eabd8ea6706', 'shihab', 'learner', 'shihab.learner@mail.com', '$2y$10$sI30TgE0wh.BwD0/sSPFjOQpiM8Od7mYKM4pVra.WE7bbiR2cTukq', 'pictures/profile/default.png', 'learner'),
 ('61eac18477237', 'fady', 'learner', 'fady@gmail.com', '$2y$10$6cgcsNhmsXfHYxKYg.H2vejo1pB70rKDCAMPdu.Pex4liX7IAXDT.', 'pictures/profile/default.png', 'learner'),
-('61eb1f9760bb7', 'learnertest', 'learnertest', 'learnertest@gmail.com', '$2y$10$A0WGN7y0LRst4bW5x85J4evxhN1D4NbNzi.IH9EkFA1pvvLQ6QRbW', 'pictures/profile/default.png', 'learner');
+('61eb1f9760bb7', 'learnertest', 'learnertest', 'learnertest@gmail.com', '$2y$10$A0WGN7y0LRst4bW5x85J4evxhN1D4NbNzi.IH9EkFA1pvvLQ6QRbW', 'pictures/profile/default.png', 'learner'),
+('61eb3a15352f7', 'ahmed', 'kamal', 'ahmed.kamal@gmail.com', '$2y$10$f4FQIh8qEecQMfFlKTDUiOMAT5ibDQStDX.PbYJwdsBP7wfxQs4me', 'pictures/profile/drow_ranger_dota_2_art_95109_1920x1080.jpg', 'learner');
 
 -- --------------------------------------------------------
 
@@ -143,7 +144,11 @@ CREATE TABLE `materials` (
 --
 
 INSERT INTO `materials` (`courseID`, `materialPath`) VALUES
-(4, 'materials/Sheet 4.2 Networks.pdf');
+(4, 'materials/Sheet 4.2 Networks.pdf'),
+(4, 'materials/e6a6eff912969541.jpg'),
+(4, 'materials/e6a6eff912969541.jpg'),
+(3, 'materials/R.png'),
+(5, 'materials/TechnicLauncher.exe');
 
 -- --------------------------------------------------------
 
@@ -156,19 +161,18 @@ CREATE TABLE `messages` (
   `sent_by` varchar(255) NOT NULL,
   `received_by` varchar(255) NOT NULL,
   `message` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `createdAt` varchar(255) CHARACTER SET latin1 NOT NULL
+  `createdAt` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `seen` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `messages`
 --
 
-INSERT INTO `messages` (`id`, `sent_by`, `received_by`, `message`, `createdAt`) VALUES
-(120, '61eaba8c4aac3', '61eabd8ea6706', 'hi', '2022-01-21 03:21:10pm'),
-(121, '61eaba8c4aac3', '61eac18477237', 'hello', '2022-01-21 03:23:22pm'),
-(122, '61eac18477237', '61eaba8c4aac3', 'hi', '2022-01-21 03:23:37pm'),
-(123, '61eaba8c4aac3', '61eac18477237', 'alo', '2022-01-21 03:47:35pm'),
-(124, '61eac18477237', '61eaba8c4aac3', 'hi', '2022-01-21 06:20:50pm');
+INSERT INTO `messages` (`id`, `sent_by`, `received_by`, `message`, `createdAt`, `seen`) VALUES
+(126, '61eb3a15352f7', '61eaba8c4aac3', 'alo', '2022-01-22 12:00:56am', 1),
+(127, '61eaba8c4aac3', '61eb3a15352f7', 'hla', '2022-01-22 12:01:32am', 1),
+(128, '61eaba8c4aac3', '61eb3a15352f7', 'what ?', '2022-01-22 12:01:51am', 1);
 
 -- --------------------------------------------------------
 
@@ -193,7 +197,12 @@ INSERT INTO `orders` (`orderID`, `courseID`, `learnerID`, `total`) VALUES
 (841011446, 3, '61eac18477237', 105000),
 (841011446, 4, '61eac18477237', 105000),
 (1339974304, 3, '61eb1f9760bb7', 105000),
-(1339974304, 4, '61eb1f9760bb7', 105000);
+(1339974304, 4, '61eb1f9760bb7', 105000),
+(527301535, 4, '61eac18477237', 100000),
+(26405213, 3, '61eac18477237', 5000),
+(412798758, 3, '61eb3a15352f7', 105000),
+(412798758, 4, '61eb3a15352f7', 105000),
+(85507140, 5, '61eb3a15352f7', 7000);
 
 -- --------------------------------------------------------
 
@@ -205,16 +214,18 @@ CREATE TABLE `selectedcourses` (
   `courseID` int(11) NOT NULL,
   `learnerID` varchar(255) NOT NULL,
   `rating` int(11) DEFAULT NULL,
-  `review` text DEFAULT NULL
+  `review` text DEFAULT NULL,
+  `survey` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `selectedcourses`
 --
 
-INSERT INTO `selectedcourses` (`courseID`, `learnerID`, `rating`, `review`) VALUES
-(3, '61eb1f9760bb7', NULL, NULL),
-(4, '61eb1f9760bb7', NULL, NULL);
+INSERT INTO `selectedcourses` (`courseID`, `learnerID`, `rating`, `review`, `survey`) VALUES
+(3, '61eb3a15352f7', 4, 'zby', 0),
+(4, '61eb3a15352f7', NULL, NULL, 0),
+(5, '61eb3a15352f7', NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -303,13 +314,13 @@ ALTER TABLE `auditors`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `courseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `courseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
 
 --
 -- AUTO_INCREMENT for table `tutors`
